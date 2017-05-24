9a55ca65c69d7e06697198c1b30807d47bc895d5
mohan31 <31mohan31@gmail.com>
2017-05-25 00:06:10 +0530

    testing camera

--- app/src/main/java/com/example/raghu/first/MainActivity.java
+++ app/src/main/java/com/example/raghu/first/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.raghu.first;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +24,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.LinearLayout.LayoutParams;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -26,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     LayoutParams layoutparams;
     TextView textview;
     LinearLayout relativeLayout;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
 
     @Override
@@ -39,14 +52,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               /* Snackbar.make(view, "Replace with your own action mohan", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+               // Snackbar.make(view, "Replace with your own action mohan", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
                // context = getApplicationContext();
 
                 //relativeLayout = (LinearLayout)findViewById(R.id.content_main);
 
                 //CreateCardViewProgrammatically();
-
+                dispatchTakePictureIntent();
 
 
             }
@@ -57,8 +70,62 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
 
-    
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.raghu.first",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+                galleryAddPic();
+            }
+        }
+    }
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir =  new File(Environment
+                .getExternalStorageDirectory()
+                + File.separator
+                + "/cn/");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        System.out.println("dir="+storageDir);
+        System.out.println("file="+mCurrentPhotoPath);
+        System.out.println("path="+getFilesDir().getAbsolutePath());
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
